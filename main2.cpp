07d23e52bb85aa13c53db3ec517f9db7bdcc3c38
Frogost <strongdsi@gmail.com>
2018-11-01 21:05:23 +0300

    Ex3

---
+++ main2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <math.h>
+
+class point{
+private:
+    float x;
+    float y;
+
+public:
+    void setX(float value){
+        x=value;
+    }
+
+    void setY(float value){
+        y=value;
+    }
+
+    float getX() {
+        return x;
+    }
+    float getY() {
+        return y;
+    }
+};
+
+class polar{
+private:
+    double r;
+    double phi;
+
+public:
+    void setCord(point a1) {
+        r = sqrt(pow(a1.getX(), 2) + pow(a1.getY(), 2));
+        phi = atan(a1.getX()/a1.getY());
+    }
+    void print() {
+        std::cout << "Радиус - " << r << '\n';
+        std::cout << "Угол - " << phi << '\n';
+    }
+};
+
+int main() {
+    point a;
+    a.setX(1);
+    a.setY(1);
+    polar b;
+    b.setCord(a);
+    b.print();
+    return 0;
+}
